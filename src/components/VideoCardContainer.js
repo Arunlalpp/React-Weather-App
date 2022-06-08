632865305cdbd793fa11ddd8da816964f7cc1375
arunlalpp <arunlalpp@gmail.com>
2022-06-08 14:14:27 +0530

    Feat:"Created the video card for mobile"

--- src/components/VideoCardContainer.js
+++ src/components/VideoCardContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {ImPlay2} from 'react-icons/im'
 
 export default function VideoCardContainer({ title }) {
   return (
     <>
       <div className="bg-white rounded-lg mt-2 lg:w-[52%] lg:ml-[12.5%] w-9xl ml-[3.3%]">
         <span className="font-bold ml-5 mt-4">{title}</span>
-        <div className="grid lg:grid-cols-4 gap-4 p-5">
+        <div className="md:grid md:grid-cols-4 gap-4 p-5 hidden ">
           <div>
             <div>
               <img
@@ -51,8 +52,23 @@ export default function VideoCardContainer({ title }) {
             </div>
           </div>
         </div>
-        <button className=" ml-6 mb-4 inline-block mt-6 rounded-full px-6 py-2.5 bg-[#1B4DE4] text-white font-medium text-xs leading-tight  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Show more</button>
+        <div className="p-5">
+          <div>
+            <div className="w-full relative">
+              <img
+                className="rounded-lg m-0 p-0 w-[100%]"
+                src="https://v.w-x.co/1654260720403_BIGGEST_PLANT_ON_EARTH2_mezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60" alt="card-title"
+              />
+              <ImPlay2 className="absolute top-[40%] left-[40%] text-[4rem] fill-white" />
+            </div>
+            <div className="mt-2">
+              <p className="md:text-[14px] text-[16px] hover:text-blue-500 cursor-pointer">This Plant in Australia Covers An Area Four Times the Size of New Delhi!</p>
+            </div>
+          </div>
       </div>
+      <button className=" ml-6 mb-4 inline-block mt-2 rounded-full px-6 py-2.5 bg-[#1B4DE4] text-white font-medium text-xs leading-tight  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Show more</button>
+      </div>
+        <button className="md:block hidden ml-6 mb-4 inline-block mt-6 rounded-full px-6 py-2.5 bg-[#1B4DE4] text-white font-medium text-xs leading-tight  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Show more</button>
     </>
   );
 }
