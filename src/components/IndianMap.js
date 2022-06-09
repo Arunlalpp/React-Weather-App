3cbf5b429e76612ecb3d16fcaa8906f17dbd3c63
arunlalpp <arunlalpp@gmail.com>
2022-06-10 02:45:31 +0530

    Image alt included

--- src/components/IndianMap.js
+++ src/components/IndianMap.js
@@ -6,29 +6,30 @@ import VideoCardContainer from "./VideoCardContainer";
 
 export default function IndianMap() {
   return (
-    <>
-      <div className="lg:w-[52%] lg:ml-[12.5%] w-9xl ml-[3.3%] md:pt-0 pt-3">
-        <div className="p-3 rounded-t-lg bg-white">
-          <span className="text-[#2b2b2b] text-xl font-bold">
-            Weather Across India
-          </span>
-          <img className="rounded-lg mt-3"
-            src="https://s.w-x.co/in-saturday.jpg?crop=16:9&width=980&format=pjpg&auto=webp&quality=60"
-            alt="indian-map"
-          />
-          <div>
-            <p className="text-[#2B2B2B] md:text-2xl text-lg  md:font-bold font-normal leading-[1.4] mb-4 mt-3 cursor-pointer hover:text-blue-500">
-              Weekend Weather (June 4-5): Meghalaya, Assam, West Bengal, Sikkim,
-              Karnataka, Tamil Nadu and Kerala In for Heavy Rains
-            </p>
-          </div>
-        </div>
-      </div>
+		<>
+			<div className="lg:w-[52%] lg:ml-[12.5%] w-9xl ml-[3.3%] md:pt-0 pt-3">
+				<div className="p-3 rounded-t-lg bg-white">
+					<span className="text-[#2b2b2b] text-xl font-bold">
+						Weather Across India
+					</span>
+					<img
+						className="rounded-lg mt-3"
+						src="https://s.w-x.co/in-saturday.jpg?crop=16:9&width=980&format=pjpg&auto=webp&quality=60"
+						alt="IndianMap"
+					/>
+					<div>
+						<p className="text-[#2B2B2B] md:text-2xl text-lg  md:font-bold font-normal leading-[1.4] mb-4 mt-3 cursor-pointer hover:text-blue-500">
+							Weekend Weather (June 4-5): Meghalaya, Assam, West Bengal, Sikkim,
+							Karnataka, Tamil Nadu and Kerala In for Heavy Rains
+						</p>
+					</div>
+				</div>
+			</div>
 
-      <CardTitle />
-      <SmallCard />
-      <VideoCardContainer title="Top Video" />
-      <Footer />
-    </>
-  );
+			<CardTitle />
+			<SmallCard />
+			<VideoCardContainer title="Top Video" />
+			<Footer />
+		</>
+	);
 }
--- src/components/VideoCardContainer.js
+++ src/components/VideoCardContainer.js
@@ -3,71 +3,89 @@ import {ImPlay2} from 'react-icons/im'
 
 export default function VideoCardContainer({ title }) {
   return (
-    <>
-      <div className="bg-white rounded-lg mt-2 lg:w-[52%] lg:ml-[12.5%] w-9xl ml-[3.3%]">
-        <span className="font-bold ml-5 mt-4">{title}</span>
-        <div className="md:grid md:grid-cols-4 gap-4 p-5 hidden ">
-          <div>
-            <div>
-              <img
-                className="rounded-lg h-[15vh]"
-                src="https://v.w-x.co/1654260720403_BIGGEST_PLANT_ON_EARTH2_mezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60" alt="card-title"
-              />
-            </div>
-            <div className="mt-2">
-              <p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">This Plant in Australia Covers An Area Four Times the Size of New Delhi!</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img
-                className="rounded-lg h-[15vh]"
-                src="https://v.w-x.co/1654057617138_0530_BRAZIL_FLOODING_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"  alt="card-title"
-              />
-            </div>
-            <div className="mt-2">
-              <p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">Brazil Flooding Kills Dozens, Leaves Thousands Homeless</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img
-                className="rounded-lg h-[15vh]"
-                src="https://v.w-x.co/1653912618674_0526_EarthIsLosingITsBioCrust_Text_Editmezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"  alt="card-title"
-              />
-            </div>
-            <div className="mt-2">
-              <p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">Places Getting Dusty: Little-Known Potential Impact of Climate Change</p>
-            </div>
-          </div>
-          <div>
-            <div>
-              <img
-                className="rounded-lg h-[15vh]"
-                src="https://v.w-x.co/1654483906949_India_2_Day_TOMORROW_Jun_5_2022_06_10_33AMproxy.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"  alt="card-title"
-              />
-            </div>
-            <div className="mt-2">
-              <p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">WATCH: Latest India Weather Forecast: June 6</p>
-            </div>
-          </div>
-        </div>
-        <div className="p-5 md:hidden">
-          <div>
-            <div className="w-full relative">
-              <img
-                className="rounded-lg m-0 p-0 w-[100%]"
-                src="https://v.w-x.co/1654260720403_BIGGEST_PLANT_ON_EARTH2_mezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60" alt="card-title"
-              />
-              <ImPlay2 className="absolute top-[40%] left-[40%] text-[4rem] fill-white" />
-            </div>
-            <div className="mt-2">
-              <p className="md:text-[14px] text-[16px] hover:text-blue-500 cursor-pointer">This Plant in Australia Covers An Area Four Times the Size of New Delhi!</p>
-            </div>
-          </div>
-      </div>
-      <button className="md:hidden block ml-6 mb-4 mt-2 rounded-full px-6 py-2.5 bg-[#1B4DE4] text-white font-medium text-xs leading-tight  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Show more</button>
-      </div>
-    </>
-  );
+		<>
+			<div className="bg-white rounded-lg mt-2 lg:w-[52%] lg:ml-[12.5%] w-9xl ml-[3.3%]">
+				<span className="font-bold ml-5 mt-4">{title}</span>
+				<div className="md:grid md:grid-cols-4 gap-4 p-5 hidden ">
+					<div>
+						<img
+							className="rounded-lg h-[15vh]"
+							src="https://v.w-x.co/1654260720403_BIGGEST_PLANT_ON_EARTH2_mezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"
+							alt="card-title"
+						/>
+						<div className="mt-2">
+							<p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">
+								This Plant in Australia Covers An Area Four Times the Size of
+								New Delhi!
+							</p>
+						</div>
+					</div>
+					<div>
+						<div>
+							<img
+								className="rounded-lg h-[15vh]"
+								src="https://v.w-x.co/1654057617138_0530_BRAZIL_FLOODING_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"
+								alt="card-title"
+							/>
+						</div>
+						<div className="mt-2">
+							<p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">
+								Brazil Flooding Kills Dozens, Leaves Thousands Homeless
+							</p>
+						</div>
+					</div>
+					<div>
+						<div>
+							<img
+								className="rounded-lg h-[15vh]"
+								src="https://v.w-x.co/1653912618674_0526_EarthIsLosingITsBioCrust_Text_Editmezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"
+								alt="card-title"
+							/>
+						</div>
+						<div className="mt-2">
+							<p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">
+								Places Getting Dusty: Little-Known Potential Impact of Climate
+								Change
+							</p>
+						</div>
+					</div>
+					<div>
+						<div>
+							<img
+								className="rounded-lg h-[15vh]"
+								src="https://v.w-x.co/1654483906949_India_2_Day_TOMORROW_Jun_5_2022_06_10_33AMproxy.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"
+								alt="card-title"
+							/>
+						</div>
+						<div className="mt-2">
+							<p className="md:text-[14px] text-[10px] hover:text-blue-500 cursor-pointer">
+								WATCH: Latest India Weather Forecast: June 6
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="p-5 md:hidden">
+					<div>
+						<div className="w-full relative">
+							<img
+								className="rounded-lg m-0 p-0 w-[100%]"
+								src="https://v.w-x.co/1654260720403_BIGGEST_PLANT_ON_EARTH2_mezz_HighRes_avc_mpeg4_6000kbps.jpg?crop=16:9&width=160&format=pjpg&auto=webp&quality=60"
+								alt="card-title"
+							/>
+							<ImPlay2 className="absolute top-[40%] left-[40%] text-[4rem] fill-white" />
+						</div>
+						<div className="mt-2">
+							<p className="md:text-[14px] text-[16px] hover:text-blue-500 cursor-pointer">
+								This Plant in Australia Covers An Area Four Times the Size of
+								New Delhi!
+							</p>
+						</div>
+					</div>
+				</div>
+				<button className="md:hidden block ml-6 mb-4 mt-2 rounded-full px-6 py-2.5 bg-[#1B4DE4] text-white font-medium text-xs leading-tight  hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+					Show more
+				</button>
+			</div>
+		</>
+	);
 }
